a4b50a2c9c7a2e13c54b199fe9891385a3e08523
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-07-20 07:32:17 -0700

    Remove arch bootstrap.

--- scripts/arch.sh
+++
@@ -1,9 +0,0 @@
-# Arch Linux bootstrap configuration
-
-# Install packages
-sudo pacaur -S --noconfirm --noedit \
-xorg-server xorg-xinit i3 feh dos2unix \
-xrandr lxrandr dmenu conky mesa mesa-demos \
-fasd git termite zsh xorg-twm python3 \
-i3blocks cmake bash zsh ttf-hack emacs neovim \
-google-chrome neofetch python2-neovim python-neovim
