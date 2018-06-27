4376e99ef64076b6f5872f6d0a99109839b68abb
Matthew J. Berger <matthewberger@nevada.unr.edu>
2018-06-27 10:41:42 -0700

    Don't use nitrogen for setting wallpaper. Delete unused wallpapers.

--- scripts/wallpaper-commands.sh
+++ scripts/wallpaper-commands.sh
@@ -1,9 +1,10 @@
 set-wallpaper() {
-	wal -n -i "$@"
-	nitrogen --save --set-zoom-fill "$(< "${HOME}/.cache/wal/wal")"
+	wal -i "$@"
+	. ~/.dotfiles/bspwmrc > /dev/null
 }
 
 set-random-wallpaper() {
 	wallpaper=$(ls $HOME/.dotfiles/wallpapers | shuf -n 1)
 	set-wallpaper "$HOME/.dotfiles/wallpapers/$wallpaper"
+	. ~/.dotfiles/bspwmrc > /dev/null
 }
