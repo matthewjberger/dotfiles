0ecd2a96073079438341d50f45d86834393442f8
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-05-19 16:47:16 -0700

    Make setup environment idempotent. Remove blackarch install

--- bootstrap/setup-environment.sh
+++ bootstrap/setup-environment.sh
@@ -1,15 +1,19 @@
 # Install an aur helper
-cd $HOME
-git clone https://aur.archlinux.org/yay.git
-cd yay
-yes | makepkg -sri
+command -v yay >/dev/null 2>&1 || {
+	cd $HOME
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	yes | makepkg -sri
+}
 
 # Install dotfiles
-echo "Installing dotfiles..."
-mkdir -p $HOME/code
-git clone https://github.com/matthewjberger/dotfiles $HOME/code/dotfiles --recursive
-cd $HOME/code/dotfiles
-./install
+if [ ! -d "$HOME/code/dotfiles" ]; then
+	echo "Installing dotfiles..."
+	mkdir -p $HOME/code
+	git clone https://github.com/matthewjberger/dotfiles $HOME/code/dotfiles --recursive
+	cd $HOME/code/dotfiles
+	./install
+fi
 
 # Install AUR packages
 yay -S --noconfirm \
@@ -17,10 +21,8 @@ yay -S --noconfirm \
     brogue \
     browsh \
     dfhack-twbt-bin \
-    dwarffortress-ironhand \
     dwarftherapist \
     fisher \
-    fortune-mod-calvin \
     fortune-mod-firefly \
     imgur-screenshot-git \
     nerd-fonts-hack \
@@ -32,14 +34,9 @@ yay -S --noconfirm \
     tome4-git \
 
 # Install spacemacs
-git clone https://github.com/syl20bnr/spacemacs -b develop $HOME/.emacs.d
+if [ ! -d "$HOME/.emacs.d" ]; then
+	git clone https://github.com/syl20bnr/spacemacs -b develop $HOME/.emacs.d
+fi
 
 systemctl --user enable --now emacs
-
-# Setup blackarch repo and package manager for pentesting tools
-curl -O https://blackarch.org/strap.sh
-chmod +x strap.sh
-sudo ./strap.sh
-rm strap.sh
-yay -S blackman
 
