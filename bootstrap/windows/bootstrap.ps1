7686d24bf264e2dfd00e2cb355d22ef0962ae0ce
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-08-12 13:45:44 -0700

    Update windows bootstrap

--- bootstrap/windows/bootstrap.ps1
+++ bootstrap/windows/bootstrap.ps1
@@ -8,14 +8,20 @@ set-executionpolicy unrestricted -s cu
 scoop install 7zip git innounp
 scoop bucket add extras
 scoop bucket add games
+scoop bucket add nerd-fonts
 
 scoop install `
+  adom `
+  alacritty `
+  angband `
   bat `
   bitwarden `
   brave `
+  brogue `
   busybox `
   cataclysm-dda `
   cataclysm-dda-tiles `
+  ccat `
   ccleaner `
   cmake `
   cmder `
@@ -23,15 +29,20 @@ scoop install `
   discord `
   dwarf-fortress-lnp `
   emacs `
+  flux `
   fzf `
   gcc `
+  gitkraken `
   grep `
   hack-font `
+  hub `
   keypirinha `
   less `
   lf `
+  nethack `
   nodejs `
   openssh `
+  rapidee `
   ripgrep `
   rustup `
   s `
@@ -41,9 +52,14 @@ scoop install `
   stone-soup-tiles `
   sudo `
   sumatrapdf `
-  wiztree `
+  tome4 `
+  umoria `
+  vscode `
 
 [environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+
+# Setup spacemacs
+git clone https://github.com/syl20bnr/spacemacs $env:APPDATA/.emacs.d -b develop
 
 # Setup rust
 rustup default stable
