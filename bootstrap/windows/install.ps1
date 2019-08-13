4219aa41e97092771ba4fb6aaaf2fe8e493ab1b9
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-08-13 10:20:24 -0700

    Add powershell profile

--- bootstrap/windows/install.ps1
+++ bootstrap/windows/install.ps1
@@ -3,7 +3,8 @@ function CreateSymlink([string]$source, [string]$dest)
 { 
   New-Item -ItemType SymbolicLink -Path $dest -Target $source -Force | Out-Null 
 }
-CreateSymlink ..\..\gitconfig ~/.gitconfig
-CreateSymlink ..\..\spacemacs $env:APPDATA/.spacemacs
-CreateSymlink ..\..\alacritty.yml $env:USERPROFILE/alacritty.yml
-CreateSymlink vsvimrc.vim $env:USERPROFILE/vsvimrc.vim
+CreateSymlink ..\..\gitconfig ~\.gitconfig
+CreateSymlink ..\..\spacemacs $env:APPDATA\.spacemacs
+CreateSymlink ..\..\alacritty.yml $env:USERPROFILE\alacritty.yml
+CreateSymlink vsvimrc.vim $env:USERPROFILE\vsvimrc.vim
+CreateSymlink profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
---
+++ bootstrap/windows/profile.ps1
@@ -0,0 +1,5 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
