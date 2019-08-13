7497bc013dd2d90487d6aa7b344138ed595a030f
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-08-13 10:30:41 -0700

    Add aliases

--- bootstrap/windows/profile.ps1
+++ bootstrap/windows/profile.ps1
@@ -3,3 +3,9 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+# Setup batch aliases
+DOSKEY py = python
+
+# Setup powershell aliases
+Set-Alias -Name py -Value python
